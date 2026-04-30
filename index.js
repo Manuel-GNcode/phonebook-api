e89c44d392cac6432b76ae72d9118fb090fb3ffa
GNcode <gngalarga027@gmail.com>
2026-04-30 08:20:31 -0500

    3.2: Backend de la Agenda Telefónica, paso 2

--- index.js
+++ index.js
@@ -28,6 +28,15 @@ app.get('/api/persons', (req, res) => {
   res.json(phones)
 })
 
+app.get('/info', (req, res) => {
+  res.send(`
+    <div>
+      <p>Phonebook has info for ${phones.length} people</p>
+      <p>${Date()}</p>
+    </div>
+    `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
